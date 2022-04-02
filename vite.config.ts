129d383d78828a174e6c25c23504868480f14ced
Nikolai Nolan <atlassian@nikol.ai>
2022-04-02 09:59:26 -0500

    Minify HTML

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import { createHtmlPlugin } from 'vite-plugin-html'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 export default {
   plugins: [
     pluginRewriteAll(),
+    createHtmlPlugin(),
   ],
 }
